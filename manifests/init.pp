89f75c753c7d549090259adad36e29b2120116c4
nbentoumi <nbentoumi@users.noreply.github.com>
2016-04-04 16:36:54 -0400

    Update init.pp
    Validate nsswitch_default_project only for Solaris

--- manifests/init.pp
+++ manifests/init.pp
@@ -231,12 +231,13 @@ class nsswitch (
   if $nsswitch_prof_attr_real != undef {
     validate_string($nsswitch_prof_attr_real)
   }
-
+if ( $::osfamily == 'Solaris' ) {
   if $nsswitch_project == 'USE_DEFAULTS' {
     $nsswitch_project_real = $default_nsswitch_project
   } else {
     $nsswitch_project_real = $nsswitch_project
   }
+}
 
   if $nsswitch_project_real != undef {
     validate_string($nsswitch_project_real)
